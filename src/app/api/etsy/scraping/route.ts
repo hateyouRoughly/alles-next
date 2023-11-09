cfd2b5739fc8cae009b6223825e84cbd6237a946
Deepak Singh <deepak.singh@intimetec.com>
2023-11-09 13:19:53 +0530

    fix: master dynamic route api fixed

--- src/app/api/etsy/scraping/route.ts
+++ src/app/api/etsy/scraping/route.ts
@@ -1,5 +1,7 @@
 import { scrap } from '@/etsy/scraping';
 
+export const dynamic = 'force-dynamic'
+
 export async function GET(request: Request) {
     const data = await scrap();
     return Response.json(data);
